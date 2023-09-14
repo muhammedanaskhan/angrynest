1b564827779436e4f2ea17413f94dc1e46fa3fc5
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-09-14 11:08:24 +0530

    :art: minor

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,6 +1,13 @@
 function RightSidebar(){
     return(
-        <h1>Right Side bar</h1>
+        <section className="custome-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+            </div>
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggested Users </h3>
+            </div>
+        </section>
     )
 }
 
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
-
+import {dark} from "@clerk/themes";
 function Topbar() {
     return (
         <nav className='topbar'>
@@ -22,6 +22,7 @@ function Topbar() {
                 </div>
                 <OrganizationSwitcher
                     appearance={{
+                        baseTheme: dark,
                         elements: {
                             organizationSwitcherTrigger: "py-2 px-4"
                         }
