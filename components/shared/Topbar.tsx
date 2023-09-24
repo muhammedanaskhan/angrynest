f629259252ca816989445675855aa3c90ac7be16
muhammedanaskhan <muhammedanas5186@gmail.com>
2023-09-24 15:10:36 +0530

    :art: profile dropdown added

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,8 +1,27 @@
+
+
 import Image from "next/image";
 import Link from "next/link";
-import { SignedIn, SignOutButton, OrganizationSwitcher } from "@clerk/nextjs";
-import {dark} from "@clerk/themes";
-function Topbar() {
+import { SignedIn, SignOutButton, OrganizationSwitcher, currentUser } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import { fetchUser } from "@/lib/actions/user.actions";
+  
+async function Topbar() {
+
+    const user = await currentUser();
+    if(!user)return null;
+
+    console.log("user",user)
+    const userInfo = await fetchUser(user.id);
+
     return (
         <nav className='topbar'>
             <Link href='/' className="flex items-center gap-2">
@@ -10,7 +29,8 @@ function Topbar() {
                 <p className="text-heading3-bold text-dark max-xs:hidden">Communet</p>
             </Link>
 
-            <div className="flex items-center gap-1">
+
+            <div className="flex items-center gap-2">
                 <div className="block md:hidden">
                     <SignedIn>
                         <SignOutButton>
@@ -20,6 +40,19 @@ function Topbar() {
                         </SignOutButton>
                     </SignedIn>
                 </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                    <Image src={userInfo.image} width={32} height={32} alt='logo' className="rounded-full object-cover" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <Link href={`profile/${user.id}`}><DropdownMenuItem>Profile</DropdownMenuItem>
+                        </Link>
+                        <DropdownMenuItem>Log out</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
             </div>
         </nav>
     )
